61d08e68590f1261d02a54da520c7d82920b2285
RahulSharma02 <rahulfangalia1234@gmail.com>
2020-08-14 00:19:29 +0530

    first commit

--- portfoliomaterialize/settings.py
+++ portfoliomaterialize/settings.py
@@ -13,7 +13,7 @@ import dj_database_url
 import django_heroku
 import os
 
-
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,7 +138,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
